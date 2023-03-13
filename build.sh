dd44b83940cde0cb59e20ee8b1ac5d157d960c61
ubuntunux <ubuntunux@gmail.com>
2023-03-13 23:45:07 +0900

    buildozer setting

---
+++ build.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export ANDROID_HOME=/home/ubuntunux/.buildozer/android/platform/android-sdk
+unset ANDROID_SDK_HOME
+buildozer android deploy run
