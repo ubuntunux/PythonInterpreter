0723b02b259aec0b03cbc22292470ec488444139
ubuntunux <ubuntunux@gmail.com>
2023-03-14 00:35:50 +0900

    update buildozer

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export ANDROID_HOME=/home/ubuntunux/.buildozer/android/platform/android-sdk
 unset ANDROID_SDK_HOME
-buildozer android deploy run
+adb logcat -c
+buildozer android debug deploy run logcat | grep python
